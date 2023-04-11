10390d0b3436c76543d9e968d65508abc0a9764b
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-11 12:02:40 -0300

    updated tailsind config -> add colors and fontFamily in doc

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
    theme: {
+      colors: {
+         // Primary
+         darkBlue: 'hsl(233, 26%, 24%)',
+         limeGreen: 'hsl(136, 65%, 51%)',
+         brightCyan: 'hsl(192, 70%, 51%)',
+
+         // Neutral
+         grayishBlue: 'hsl(233, 8%, 62%)',
+         lightGrayishBlue: 'hsl(220, 16%, 96%)',
+         veryLightGray: 'hsl(0, 0%, 98%)',
+         white: 'hsl(0, 0%, 100%)',
+      },
+
       extend: {},
+
+      fontFamily: {
+         sans: ['Public Sans', 'sans-serif'],
+      },
+
+      screens: {
+         smallDevice: { max: '320px' },
+         mediumDevice: { min: '375px', max: '424px' },
+         largerDevice: { min: '425px', max: '767px' },
+         tablet: { min: '768px', max: '1023px' },
+      },
    },
    plugins: [],
 };
