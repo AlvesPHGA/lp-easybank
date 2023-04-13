f11d51ff09d36b82a0ada5a1fdac0e25671cd103
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-13 15:39:03 -0300

    updated button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Button = () => {
-   return <div>Button</div>;
+   return (
+      <button className="border px-9 py-3 rounded-3xl bg-gradient-to-r from-limeGreen from-50 to-brightCyan font-bold text-white hover:opacity-80 hover:duration-500">
+         Request Invite
+      </button>
+   );
 };
 
 export default Button;
--- src/pages/includes/Global/Header.tsx
+++ src/pages/includes/Global/Header.tsx
@@ -1,47 +1,18 @@
 import React from 'react';
 
 import logoLP from '../../../assets/svg/logo.svg';
+import NavHeader from '../../../components/NavHeader';
+import Button from '../../..//components/Button';
 
 const Header = () => {
    return (
       <header className="h-24 border flex">
-         <div className="w-1200 border m-auto flex justify-between items-center">
+         <div className="w-1200 m-auto flex justify-between items-center">
             <div className="flex h-7">
                <img className="cursor-pointer" src={logoLP} alt="Logo site" />
             </div>
-            <nav>
-               <a
-                  className="font-bold text-grayishBlue hover:text-darkBlue hover:duration-500"
-                  href=""
-               >
-                  Home
-               </a>
-               <a
-                  className="m-8 font-bold text-grayishBlue hover:text-darkBlue hover:duration-500"
-                  href=""
-               >
-                  About
-               </a>
-               <a
-                  className="font-bold text-grayishBlue hover:text-darkBlue hover:duration-500"
-                  href=""
-               >
-                  Contact
-               </a>
-               <a
-                  className="m-8 font-bold text-grayishBlue hover:text-darkBlue hover:duration-500"
-                  href=""
-               >
-                  Blog
-               </a>
-               <a
-                  className="font-bold text-grayishBlue hover:text-darkBlue hover:duration-500"
-                  href=""
-               >
-                  Careers
-               </a>
-            </nav>
-            <button>Request Invite</button>
+            <NavHeader />
+            <Button />
          </div>
       </header>
    );
