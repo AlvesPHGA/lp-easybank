4d2c49c19ca74f96bbeaa003b604c524d15b7f13
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-14 10:36:17 -0300

    start subheader layout

--- src/pages/includes/home/subheader/Subheader.tsx
+++ src/pages/includes/home/subheader/Subheader.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Button from '../../../../components/Button';
+
+import imgMockups from '../../../../assets/images/image-mockups.png';
 
 const Subheader = () => {
-   return <div>Subheader</div>;
+   return (
+      <section className="w-1200">
+         <div>
+            <h1>Next generation digital banking</h1>
+            <p>
+               Take your financial life online. Your Easybank account will be a
+               one-stop for spending, saving, budgeting, investing, and much
+               more.
+            </p>
+            <Button />
+         </div>
+         <div>
+            <div>
+               <img src={imgMockups} alt="Images devices" />
+            </div>
+         </div>
+      </section>
+   );
 };
 
 export default Subheader;
