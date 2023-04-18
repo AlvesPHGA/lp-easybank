974ed1f5c81f376754039432f8468783020d647d
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-18 11:28:46 -0300

    updated card by lastArticle section

--- src/pages/includes/home/latestArticles/Card.tsx
+++ src/pages/includes/home/latestArticles/Card.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const Card = () => {
-   return <div>Card</div>;
+const Card = ({
+   img,
+   author,
+   title,
+   content,
+}: {
+   img: string;
+   author: string;
+   title: string;
+   content: string;
+}) => {
+   return (
+      <div>
+         <div></div>
+         <div>
+            <span></span>
+            <h3></h3>
+            <p></p>
+         </div>
+      </div>
+   );
 };
 
 export default Card;
