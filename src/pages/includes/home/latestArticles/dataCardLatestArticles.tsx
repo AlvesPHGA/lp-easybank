31e3df809650224e430852ae39f04795beda4799
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-18 11:46:24 -0300

    was created a doc with info cards in latestArticle section

---
+++ src/pages/includes/home/latestArticles/dataCardLatestArticles.tsx
@@ -0,0 +1,37 @@
+import imgCash from '../../../../assets/images/image-currency.jpg';
+import imgRestaurant from '../../../../assets/images/image-restaurant.jpg';
+import imgPlane from '../../../../assets/images/image-plane.jpg';
+import imgConfetti from '../../../../assets/images/image-confetti.jpg';
+
+const author = ['Claire Robinson', 'Wilson Hutton'];
+
+export const dataContentCard = [
+   {
+      img: imgCash,
+      author: author[0],
+      title: 'Receive money in any currency with no fees',
+      content:
+         "The world is getting smaller and we're becoming more mobile. So why should you be forced to only receive money in a single ()",
+   },
+   {
+      img: imgRestaurant,
+      author: author[1],
+      title: 'Treat yourself without worrying about money',
+      content:
+         'Our simple budgeting feature allows you to separate out your speding and set realistic limits each month. That means you ()',
+   },
+   {
+      img: imgPlane,
+      author: author[1],
+      title: 'Take your EasyBank card wherever you go',
+      content:
+         "We want you to enjoy your travels. This is why we don't charge any fees on purchases while you're abroad. We'll even show you ()",
+   },
+   {
+      img: imgConfetti,
+      author: author[0],
+      title: 'Our invite-only Beta accounts are now live!',
+      content:
+         "After a lot of of hard work by the whole team, we're excited to launch our closed beta. It's easy to request an invite through the site ()",
+   },
+];
