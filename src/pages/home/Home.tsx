5fd9d041abfc2af617e3ac0070074d3298fce1be
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-18 11:25:45 -0300

    start latestArticle section layout

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Subheader from '../includes/home/subheader/Subheader';
 import ChooseEasybank from '../includes/home/chooseEasybank/ChooseEasybank';
+import LatestArticle from '../includes/home/latestArticles/LatestArticle';
 
 const Home = () => {
    return (
       <>
          <Subheader />
          <ChooseEasybank />
+         <LatestArticle />
       </>
    );
 };
--- src/pages/includes/home/latestArticles/LatestArticle.tsx
+++ src/pages/includes/home/latestArticles/LatestArticle.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const LatestArticle = () => {
-   return <div>LatestArticle</div>;
+   return (
+      <section className="bg-veryLightGray py-28">
+         <section className="w-1200 mx-auto">
+            <h2 className="text-darkBlue mb-10">Latest Articles</h2>
+
+            <section>
+               <div>
+                  <div></div>
+                  <div>
+                     <span></span>
+                     <h3></h3>
+                     <p></p>
+                  </div>
+               </div>
+            </section>
+         </section>
+      </section>
+   );
 };
 
 export default LatestArticle;
