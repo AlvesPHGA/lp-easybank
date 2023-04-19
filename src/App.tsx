c59799d8ee4d773ddca41dabecd972d58024304b
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-19 16:44:15 -0300

    add footer in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Home from './pages/home/Home';
+import Footer from './pages/includes/Global/Footer';
 import Header from './pages/includes/Global/Header';
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
       <>
          <Header />
          <Home />
+         <Footer />
       </>
    );
 }
