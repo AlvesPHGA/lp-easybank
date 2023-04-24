cc7ac50a54f235b6ae520945f11f36ea250e7754
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-24 14:31:20 -0300

    updated chooseEasybank layout to mobile device

--- src/pages/includes/home/chooseEasybank/Card.tsx
+++ src/pages/includes/home/chooseEasybank/Card.tsx
@@ -12,12 +12,16 @@ const Card = ({
    content: string;
 }) => {
    return (
-      <div className="w-fit p-[0.5rem]">
-         <img src={svg} alt={title} />
+      <div className="w-fit p-[0.5rem] smallDevice:text-center">
+         <img
+            src={svg}
+            alt={title}
+            className="smallDevice:px-auto smallDevice:inline"
+         />
          <div>
             <h3 className="mt-8 mb-6 text-darkBlue">{title}</h3>
             <p
-               className="text-grayishBlue"
+               className="text-grayishBlue smallDevice:text-[1rem]"
                style={{ fontSize: `${18 / 16}rem` }}
             >
                {content}
--- src/pages/includes/home/chooseEasybank/ChooseEasybank.tsx
+++ src/pages/includes/home/chooseEasybank/ChooseEasybank.tsx
@@ -4,15 +4,17 @@ import Card from './Card';
 
 const ChooseEasybank = () => {
    return (
-      <section className="py-28 bg-lightGrayishBlue">
-         <div className="w-1200 m-auto">
-            <h2 className="text-darkBlue">Why choose Easybank?</h2>
-            <p className="text-grayishBlue w-[690px] mt-6 mb-16">
+      <section className="py-28 bg-lightGrayishBlue smallDevice:py-10">
+         <div className="w-1200 m-auto smallDevice:w-full smallDevice:px-4">
+            <h2 className="text-darkBlue smallDevice:text-4xl smallDevice:text-center">
+               Why choose Easybank?
+            </h2>
+            <p className="text-grayishBlue w-[690px] mt-6 mb-16 smallDevice:w-full smallDevice:text-base smallDevice:text-center">
                We leverage Open Banking to turn your bank account into your
                financial hub. Control your finances like never before.
             </p>
 
-            <div className="flex gap-4 justify-between">
+            <div className="flex gap-4 justify-between smallDevice:flex-col smallDevice:gap-6 smallDevice:items-center">
                {dataCardChooseEasybank.map((dt) => (
                   <Card key={dt.id} {...dt} />
                ))}
