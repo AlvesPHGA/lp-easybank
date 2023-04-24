678737c7fc2c7cc61bffd33e18b01fa9026c78d8
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-24 14:33:56 -0300

    updated latestArticle section layout to mobile device

--- src/pages/includes/home/latestArticles/LatestArticle.tsx
+++ src/pages/includes/home/latestArticles/LatestArticle.tsx
@@ -4,11 +4,11 @@ import Card from './Card';
 
 const LatestArticle = () => {
    return (
-      <section className="bg-veryLightGray py-28">
-         <section className="w-1200 mx-auto">
+      <section className="bg-veryLightGray py-28 smallDevice:py-10">
+         <section className="w-1200 mx-auto smallDevice:w-full smallDevice:px-4">
             <h2 className="text-darkBlue mb-10">Latest Articles</h2>
 
-            <section className="flex justify-between">
+            <section className="flex justify-between smallDevice:flex-col gap-6">
                {dataContentCard.map((dt) => (
                   <Card key={dt.id} {...dt} />
                ))}
