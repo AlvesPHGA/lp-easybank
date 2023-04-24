a4f4fccee4df729f0a2a2b6a13cfdf52cfc14159
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-24 11:32:23 -0300

    updated header layout to mobile device

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavHeader = () => {
    return (
-      <nav>
+      <nav className="border smallDevice:hidden smallDevice:flex-col smallDevice:bg-white smallDevice:w-[100vw] smallDevice:absolute smallDevice:top-[100px] smallDevice:h-[90vh] smallDevice:left-0">
          <a
             className="font-publicSans text-grayishBlue relative hover:text-darkBlue after:absolute  after:transparent after:w-full after:h-1 after:left-0 after:top-52 hover:after:bg-brightCyan hover:duration-500"
             href=""
--- src/pages/includes/Global/Header.tsx
+++ src/pages/includes/Global/Header.tsx
@@ -4,15 +4,17 @@ import logoLP from '../../../assets/svg/logo.svg';
 import NavHeader from '../../../components/NavHeader';
 import Button from '../../..//components/Button';
 
+import { ReactComponent as Hamburger } from '../../../assets/svg/icon-hamburger.svg';
+
 const Header = () => {
    return (
-      <header className="h-24 flex bg-white relative z-[100]">
-         <div className="w-1200 m-auto flex justify-between items-center">
+      <header className="h-24 flex bg-white relative z-[100] smallDevice:fixed smallDevice:w-[100vw] smallDevice:top-0 smallDevice:h-[100px]">
+         <div className="w-1200 m-auto flex justify-between items-center smallDevice:w-full smallDevice:px-4">
             <div className="flex h-7">
                <img className="cursor-pointer" src={logoLP} alt="Logo site" />
             </div>
+            <Hamburger />
             <NavHeader />
-            <Button />
          </div>
       </header>
    );
