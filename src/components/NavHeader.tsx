ed2a9af66b4ea99daf4720c310f1b610a2dad0ac
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-26 11:02:03 -0300

    updated links in footer

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavHeader = () => {
    return (
-      <nav className="smallDevice:flex smallDevice:flex-col smallDevice:w-[100vw] smallDevice:absolute smallDevice:top-[100px] smallDevice:h-[calc(100vh-100px)] smallDevice:left-0">
+      <nav className="smallDevice:hidden smallDevice:flex-col smallDevice:w-[100vw] smallDevice:absolute smallDevice:top-[100px] smallDevice:h-[calc(100vh-100px)] smallDevice:left-0">
          <div className="smallDevice:bg-white smallDevice:w-[90%] smallDevice:h-[70vh] smallDevice:flex smallDevice:flex-col smallDevice:gap-1 smallDevice:justify-center smallDevice:mt-[1.25rem] smallDevice:mx-auto smallDevice:items-center smallDevice:rounded-lg">
             <a
                className="font-publicSans text-grayishBlue relative hover:text-darkBlue after:absolute  after:transparent after:w-full after:h-1 after:left-0 after:top-52 hover:after:bg-brightCyan hover:duration-500 smallDevice:text-darkBlue"
