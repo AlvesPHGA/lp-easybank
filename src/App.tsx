defe539aba53dd6c3f352cc0ec1c4b5c270df054
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-26 17:56:54 -0300

    was added react-router-dom in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Footer from './pages/includes/Global/Footer';
 import Header from './pages/includes/Global/Header';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+import Blog from './pages/blog/Blog';
+import Careers from './pages/careers/Careers';
 
 export default function App() {
    return (
       <>
-         <Header />
-         <Home />
-         <Footer />
+         <BrowserRouter>
+            <Header />
+            <main>
+               <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="about/" element={<About />} />
+                  <Route path="contact/" element={<Contact />} />
+                  <Route path="blog/" element={<Blog />} />
+                  <Route path="careers/" element={<Careers />} />
+               </Routes>
+            </main>
+            <Footer />
+         </BrowserRouter>
       </>
    );
 }
--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -1,39 +1,41 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const NavHeader = () => {
    return (
       <nav className="smallDevice:hidden smallDevice:flex-col smallDevice:w-[100vw] smallDevice:absolute smallDevice:top-[100px] smallDevice:h-[calc(100vh-100px)] smallDevice:left-0">
          <div className="smallDevice:bg-white smallDevice:w-[90%] smallDevice:h-[70vh] smallDevice:flex smallDevice:flex-col smallDevice:gap-1 smallDevice:justify-center smallDevice:mt-[1.25rem] smallDevice:mx-auto smallDevice:items-center smallDevice:rounded-lg">
-            <a
+            <Link
+               to="/"
                className="font-publicSans text-grayishBlue relative hover:text-darkBlue after:absolute  after:transparent after:w-full after:h-1 after:left-0 after:top-52 hover:after:bg-brightCyan hover:duration-500 smallDevice:text-darkBlue"
-               href=""
             >
                Home
-            </a>
-            <a
+            </Link>
+            <Link
+               to="/about"
                className="m-8 font-publicSans text-grayishBlue relative hover:text-darkBlue after:absolute  after:transparent after:w-full after:h-1 after:left-0 after:top-52 hover:after:bg-brightCyan hover:duration-500 smallDevice:text-darkBlue"
-               href=""
             >
                About
-            </a>
-            <a
+            </Link>
+            <Link
+               to="contact"
                className="font-publicSans text-grayishBlue relative hover:text-darkBlue after:absolute  after:transparent after:w-full after:h-1 after:left-0 after:top-52 hover:after:bg-brightCyan hover:duration-500 smallDevice:text-darkBlue"
-               href=""
             >
                Contact
-            </a>
-            <a
+            </Link>
+            <Link
+               to="/blog"
                className="m-8 font-publicSans text-grayishBlue relative hover:text-darkBlue after:absolute  after:transparent after:w-full after:h-1 after:left-0 after:top-52 hover:after:bg-brightCyan hover:duration-500 smallDevice:text-darkBlue"
-               href=""
             >
                Blog
-            </a>
-            <a
+            </Link>
+            <Link
+               to="careers"
                className="font-publicSans text-grayishBlue relative hover:text-darkBlue after:absolute  after:transparent after:w-full after:h-1 after:left-0 after:top-52 hover:after:bg-brightCyan hover:duration-500 smallDevice:text-darkBlue"
-               href=""
             >
                Careers
-            </a>
+            </Link>
          </div>
       </nav>
    );
