ff4e514bf914009f135da620caa633c3fe2194db
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-26 17:30:28 -0300

    created a new page to example to router, this name about page and created a subheader component

---
+++ src/pages/about/About.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Subheader from '../includes/Global/Subheader';
+
+const About = () => {
+   const title = 'About';
+   return <Subheader title="About" />;
+};
+
+export default About;
---
+++ src/pages/includes/Global/Subheader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Subheader = ({ title }: { title: string }) => {
+   return (
+      <section>
+         <h1>{title}</h1>
+      </section>
+   );
+};
+
+export default Subheader;
