ee4afb03ead6b2fce95376be3a541589f62b94f3
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-27 14:47:31 -0300

    added a title and description to contact page

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Subheader from '../includes/Global/Subheader';
+import Head from '../../components/Head';
 
 const Contact = () => {
    return (
-      <div className="h-[calc(100vh-340px)]">
-         <Subheader title="Contact" />
-      </div>
+      <>
+         <Head title="Contact" description="" />
+         <div className="h-[calc(100vh-340px)]">
+            <Subheader title="Contact" />
+         </div>
+      </>
    );
 };
 
