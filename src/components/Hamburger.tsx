1c5c4ac2b0d51275f6ae65c58f51c86bee2b8618
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-27 18:34:54 -0300

    updated hamburger component

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Hamburger = () => {
    return (
-      <button className="w-[50px] h-8 border rounded px-1">
-         <span className="relative w-full h-1 rounded bg-darkBlue block before:absolute before:w-full before:h-1 before:bg-darkBlue before:border before:left-0 before:bottom-[6px] before:rounded after:absolute after:w-full after:h-1 after:bg-darkBlue after:border after:left-0 after:top-[6px] after:rounded"></span>
+      <button
+         aria-label="Menu"
+         className="hidden w-[50px] h-8 border rounded px-1 smallDevice:block"
+      >
+         <span className="relative w-full h-1 rounded bg-darkBlue block [&.active]:bg-transparent before:absolute before:w-full before:h-1 before:bg-darkBlue before:border before:left-0 before:bottom-[6px] before:rounded before:[&.active]:rotate-45 before:[&.active]:top-0 after:absolute after:w-full after:h-1 after:bg-darkBlue after:border after:left-0 after:top-[6px] after:rounded after:[&.active]:rotate-[130deg] after:[&.active]:top-0"></span>
       </button>
    );
 };
