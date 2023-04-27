4f6076ff185c34fe415d306849797f81daa7ba60
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-27 18:47:38 -0300

    updated hamburger component, was remove border

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -4,7 +4,7 @@ const Hamburger = () => {
    return (
       <button
          aria-label="Menu"
-         className="hidden w-[50px] h-8 border rounded px-1 smallDevice:block"
+         className="hidden w-[50px] h-8 rounded px-1 smallDevice:block"
       >
          <span className="relative w-full h-1 rounded bg-darkBlue block [&.active]:bg-transparent before:absolute before:w-full before:h-1 before:bg-darkBlue before:border before:left-0 before:bottom-[6px] before:rounded before:[&.active]:rotate-45 before:[&.active]:top-0 after:absolute after:w-full after:h-1 after:bg-darkBlue after:border after:left-0 after:top-[6px] after:rounded after:[&.active]:rotate-[130deg] after:[&.active]:top-0"></span>
       </button>
