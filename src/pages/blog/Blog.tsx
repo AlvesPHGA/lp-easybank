56adfb56028897679fbec1cd2b353c19d28fa8a2
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-27 14:48:50 -0300

    added a title and description to blog page

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Subheader from '../includes/Global/Subheader';
+import Head from '../../components/Head';
 
 const Blog = () => {
    return (
-      <div className="h-[calc(100vh-340px)]">
-         <Subheader title="Blog" />
-      </div>
+      <>
+         <Head title="Blog" description="" />
+         <div className="h-[calc(100vh-340px)]">
+            <Subheader title="Blog" />
+         </div>
+      </>
    );
 };
 
