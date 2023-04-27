98fa7c969a956b2e6151640fdf20ebc5da621213
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-27 11:53:18 -0300

    created a Head component

---
+++ src/components/Head.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+const Head = ({
+   title,
+   description,
+}: {
+   title: string;
+   description: string;
+}) => {
+   return (
+      <HelmetProvider>
+         <Helmet>
+            <title>{title}</title>
+            <meta name="description" content={`${description}`} />
+         </Helmet>
+      </HelmetProvider>
+   );
+};
+
+export default Head;
