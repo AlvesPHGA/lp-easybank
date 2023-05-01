8bc18d0106eaa9b6d711ad32c755d150c6c22f3b
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-01 17:07:15 -0300

    updated latestArticle home layout to mediumDevice

--- src/pages/includes/home/latestArticles/LatestArticle.tsx
+++ src/pages/includes/home/latestArticles/LatestArticle.tsx
@@ -4,13 +4,13 @@ import Card from './Card';
 
 const LatestArticle = () => {
    return (
-      <section className="bg-veryLightGray py-28 smallDevice:py-10">
-         <section className="w-1200 mx-auto smallDevice:w-full smallDevice:px-4">
-            <h2 className="text-darkBlue mb-10 smallDevice:text-center smallDevice:mb-5">
+      <section className="bg-veryLightGray py-28 smallDevice:py-10 mediumDevice:py-10">
+         <section className="w-1200 mx-auto smallDevice:w-full smallDevice:px-4 mediumDevice:w-full mediumDevice:px-4">
+            <h2 className="text-darkBlue mb-10 smallDevice:text-center smallDevice:mb-5 mediumDevice:text-center mediumDevice:mb-5">
                Latest Articles
             </h2>
 
-            <section className="flex justify-between smallDevice:flex-col gap-6">
+            <section className="flex justify-between smallDevice:flex-col gap-6 mediumDevice:flex-col">
                {dataContentCard.map((dt) => (
                   <Card key={dt.id} {...dt} />
                ))}
