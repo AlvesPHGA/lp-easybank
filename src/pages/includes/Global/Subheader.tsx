8033dd6febb4f88565c22d54ddbb48d3382d9e84
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-01 20:17:43 -0300

    updated global subheader layout to mediumDevice

--- src/pages/includes/Global/Subheader.tsx
+++ src/pages/includes/Global/Subheader.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Subheader = ({ title }: { title: string }) => {
    return (
-      <section className="py-10 bg-lightGrayishBlue smallDevice:mt-[100px]">
-         <div className="w-1200 mx-auto smallDevice:w-full smallDevice:py-4">
+      <section className="py-10 bg-lightGrayishBlue smallDevice:mt-[100px] mediumDevice:mt-[100px]">
+         <div className="w-1200 mx-auto smallDevice:w-full smallDevice:py-4 mediumDevice:w-full mediumDevice:py-4">
             <h1 className="text-center">{title}</h1>
          </div>
       </section>
