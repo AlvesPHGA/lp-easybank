0ff2adce4cb7da644dc18de8ca696b226c5b3489
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-01 17:45:05 -0300

    updated subheader layout and button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = () => {
    return (
-      <button className="cta border-none px-9 py-3 rounded-3xl font-bold text-white hover:opacity-80 hover:duration-500 smallDevice:mx-auto smallDevice:block">
+      <button className="cta border-none px-9 py-3 rounded-3xl font-bold text-white hover:opacity-80 hover:duration-500 smallDevice:mx-auto smallDevice:block mediumDevice:mx-auto mediumDevice:block">
          Request Invite
       </button>
    );
--- src/pages/includes/home/subheader/Subheader.tsx
+++ src/pages/includes/home/subheader/Subheader.tsx
@@ -5,14 +5,14 @@ import backIntroDesk from '../../../../assets/svg/bg-intro-desktop.svg';
 
 const Subheader = () => {
    return (
-      <section className="relative h-fit pt-[7rem] pb-[7.7rem] flex bg-veryLightGray z-0  ">
-         <div className="absolute bg-backDesktop right-0 z-[-1] w-[57vw] h-[100vh] top-[-215px] bg-cover bg-no-repeat bg-customPosition01 bg-custom02 smallDevice:block smallDevice:bg-backDevice smallDevice:h-[47vh] smallDevice:w-full smallDevice:relative smallDevice:top-0 smallDevice:bg-cBS_mbl01 smallDevice:bg-subHomeMobile mediumDevice:block mediumDevice:bg-backDevice mediumDevice:h-[47vh] mediumDevice:w-full mediumDevice:relative mediumDevice:top-0 mediumDevice:bg-cBS_mbl01 mediumDevice:bg-subHomeMobile"></div>
+      <section className="relative h-fit pt-[7rem] pb-[7.7rem] flex bg-veryLightGray z-0 smallDevice:flex-col smallDevice:mt-[100px] smallDevice:pt-0 smallDevice:pb-10 mediumDevice:flex-col mediumDevice:mt-[100px] mediumDevice:pt-0 mediumDevice:pb-16">
+         <div className="absolute bg-backDesktop right-0 z-[-1] w-[57vw] h-[100vh] top-[-215px] bg-cover bg-no-repeat bg-customPosition01 bg-custom02 smallDevice:block smallDevice:bg-backDevice smallDevice:h-[26.7vh] smallDevice:w-full smallDevice:relative smallDevice:top-0 smallDevice:cBS_mbl01 smallDevice:bg-center mediumDevice:block mediumDevice:bg-backDevice mediumDevice:h-[26.7vh] mediumDevice:w-full mediumDevice:relative mediumDevice:top-0 mediumDevice:cBS_mbl01 mediumDevice:bg-center"></div>
 
          <div className="w-1200 m-auto smallDevice:w-full smallDevice:px-4 smallDevice:mt-8 mediumDevice:w-full mediumDevice:px-4 mediumDevice:mt-8">
             <h1 className="text-darkBlue w-[600px] smallDevice:w-full smallDevice:text-4xl smallDevice:text-center mediumDevice:w-full mediumDevice:text-4xl mediumDevice:text-center">
                Next generation digital banking
             </h1>
-            <p className="w-[500px] text-grayishBlue my-7 smallDevice:w-full smallDevice:text-center smallDevice:my-5   mediumDevice:w-full mediumDevice:text-center mediumDevice:my-5">
+            <p className="w-[500px] text-grayishBlue my-7 smallDevice:w-full smallDevice:text-center smallDevice:my-5 mediumDevice:w-full mediumDevice:text-center mediumDevice:my-5">
                Take your financial life online. Your Easybank account will be a
                one-stop for spending, saving, budgeting, investing, and much
                more.
@@ -20,7 +20,7 @@ const Subheader = () => {
 
             <Button />
          </div>
-         <div className="absolute bg-mockup bg-ba w-[50vw] h-[840px] z-[-1] bg-no-repeat bg-custom bg-[152px] top-[-115px] posi right-0 smallDevice:h-[70vh] smallDevice:left-0 smallDevice:w-full smallDevice:bg-contain smallDevice:bg-[center] smallDevice:top-[-115px]  mediumDevice:h-[70vh] mediumDevice:left-0 mediumDevice:w-full mediumDevice:bg-contain mediumDevice:bg-[center] mediumDevice:top-[-115px]"></div>
+         <div className="absolute bg-mockup bg-ba w-[50vw] h-[840px] z-[-1] bg-no-repeat bg-custom bg-[152px] top-[-115px] posi right-0 smallDevice:h-[40vh] smallDevice:left-0 smallDevice:w-full smallDevice:bg-contain smallDevice:bg-[center] smallDevice:top-[-100px] mediumDevice:h-[40vh] mediumDevice:left-0 mediumDevice:w-full mediumDevice:bg-contain mediumDevice:bg-[center] mediumDevice:top-[-100px]"></div>
       </section>
    );
 };
