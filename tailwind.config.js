bc904060f44420148162bec377a26233cde3aae8
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-03 19:34:54 -0300

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ export default {
    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
    theme: {
       backgroundImage: {
-         backDesktop: 'url(./src/assets/svg/bg-intro-desktop.svg)',
-         backDevice: 'url(./src/assets/svg/bg-intro-mobile.svg)',
+         backDesktop: 'url(src/assets/svg/bg-intro-desktop.svg)',
+         backDevice: 'url(src/assets/svg/bg-intro-mobile.svg)',
 
          mockup: 'url(./src/assets/images/image-mockups.png)',
       },
