852c82751dee7e61b3f61cd06fe6ca4540d6ed6b
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-05 17:24:41 -0300

    updated subheader global layout to largerDevice

--- src/pages/includes/Global/Subheader.tsx
+++ src/pages/includes/Global/Subheader.tsx
@@ -1,7 +1,7 @@
 const Subheader = ({ title }: { title: string }) => {
    return (
-      <section className="py-10 bg-lightGrayishBlue smallDevice:mt-[100px] mediumDevice:mt-[100px]">
-         <div className="w-1200 mx-auto smallDevice:w-full smallDevice:py-4 mediumDevice:w-full mediumDevice:py-4">
+      <section className="py-10 bg-lightGrayishBlue smallDevice:mt-[100px] mediumDevice:mt-[100px] largerDevice:mt-[100px]">
+         <div className="w-1200 mx-auto smallDevice:w-full smallDevice:py-4 mediumDevice:w-full mediumDevice:py-4 largerDevice:w-full largerDevice:py-5">
             <h1 className="text-center">{title}</h1>
          </div>
       </section>
