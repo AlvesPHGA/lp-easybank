df006b962fe9516d6735e8a3501ff0ec43902520
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-05 11:01:52 -0300

    updated chooseEasybank section layout to larger

--- src/pages/includes/home/chooseEasybank/Card.tsx
+++ src/pages/includes/home/chooseEasybank/Card.tsx
@@ -12,18 +12,18 @@ const Card = ({
    content: string;
 }) => {
    return (
-      <div className="w-fit p-[0.5rem] smallDevice:text-center mediumDevice:text-center">
+      <div className="w-fit p-[0.5rem] smallDevice:text-center mediumDevice:text-center largerDevice:text-center">
          <img
             src={svg}
             alt={title}
-            className="smallDevice:px-auto smallDevice:inline mediumDevice:inline"
+            className="smallDevice:px-auto smallDevice:inline mediumDevice:inline largerDevice:inline"
          />
          <div>
-            <h3 className="title mt-8 mb-6 text-darkBlue smallDevice:my-5 mediumDevice:my-5">
+            <h3 className="title mt-8 mb-6 text-darkBlue smallDevice:my-5 mediumDevice:my-5 largerDevice:my-7">
                {title}
             </h3>
             <p
-               className="text-grayishBlue smallDevice:text-[1rem] mediumDevice:text-[1rem]"
+               className="text-grayishBlue smallDevice:text-[1rem] mediumDevice:text-[1rem] largerDevice:text-[1.5rem]"
                style={{ fontSize: `${18 / 16}rem` }}
             >
                {content}
--- src/pages/includes/home/chooseEasybank/ChooseEasybank.tsx
+++ src/pages/includes/home/chooseEasybank/ChooseEasybank.tsx
@@ -4,17 +4,17 @@ import Card from './Card';
 
 const ChooseEasybank = () => {
    return (
-      <section className="py-28 bg-lightGrayishBlue smallDevice:py-10 mediumDevice:py-10">
-         <div className="w-1200 m-auto smallDevice:w-full smallDevice:px-4 mediumDevice:w-full mediumDevice:px-4">
-            <h2 className="text-darkBlue smallDevice:text-4xl smallDevice:text-center mediumDevice:text-4xl mediumDevice:text-center">
+      <section className="py-28 bg-lightGrayishBlue smallDevice:py-10 mediumDevice:py-10 largerDevice:py-12">
+         <div className="w-1200 m-auto smallDevice:w-full smallDevice:px-4 mediumDevice:w-full mediumDevice:px-4 largerDevice:px-5 largerDevice:w-full">
+            <h2 className="text-darkBlue smallDevice:text-4xl smallDevice:text-center mediumDevice:text-4xl mediumDevice:text-center largerDevice:text-4xl largerDevice:text-center">
                Why choose Easybank?
             </h2>
-            <p className="text-grayishBlue w-[690px] mt-6 mb-16 smallDevice:w-full smallDevice:text-base smallDevice:text-center smallDevice:mb-10 mediumDevice:w-full mediumDevice:text-base mediumDevice:text-center mediumDevice:mb-10">
+            <p className="text-grayishBlue w-[690px] mt-6 mb-16 smallDevice:w-full smallDevice:text-base smallDevice:text-center smallDevice:mb-10 mediumDevice:w-full mediumDevice:text-base mediumDevice:text-center mediumDevice:mb-10 largerDevice:w-full largerDevice:text-center largerDevice:mb-11">
                We leverage Open Banking to turn your bank account into your
                financial hub. Control your finances like never before.
             </p>
 
-            <div className="flex gap-4 justify-between smallDevice:flex-col smallDevice:gap-8 smallDevice:items-center mediumDevice:flex-col mediumDevice:gap-8 mediumDevice:items-center">
+            <div className="flex gap-4 justify-between smallDevice:flex-col smallDevice:gap-8 smallDevice:items-center mediumDevice:flex-col mediumDevice:gap-8 mediumDevice:items-center largerDevice:flex-col largerDevice:gap-10 largerDevice:items-center">
                {dataCardChooseEasybank.map((dt) => (
                   <Card key={dt.id} {...dt} />
                ))}
