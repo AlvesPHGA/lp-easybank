26f01e6310680dcced4ca3ea53223f7c9697f6d8
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-05 11:08:15 -0300

    updated latestArticles section layout to larger

--- src/pages/includes/home/latestArticles/LatestArticle.tsx
+++ src/pages/includes/home/latestArticles/LatestArticle.tsx
@@ -4,13 +4,13 @@ import Card from './Card';
 
 const LatestArticle = () => {
    return (
-      <section className="bg-veryLightGray py-28 smallDevice:py-10 mediumDevice:py-10">
-         <section className="w-1200 mx-auto smallDevice:w-full smallDevice:px-4 mediumDevice:w-full mediumDevice:px-4">
-            <h2 className="text-darkBlue mb-10 smallDevice:text-center smallDevice:mb-5 mediumDevice:text-center mediumDevice:mb-5">
+      <section className="bg-veryLightGray py-28 smallDevice:py-10 mediumDevice:py-10 largerDevice:py-12">
+         <section className="w-1200 mx-auto smallDevice:w-full smallDevice:px-4 mediumDevice:w-full mediumDevice:px-4 largerDevice:px-4 largerDevice:w-full">
+            <h2 className="text-darkBlue mb-10 smallDevice:text-center smallDevice:mb-5 mediumDevice:text-center mediumDevice:mb-5 largerDevice:text-center largerDevice:mb-7">
                Latest Articles
             </h2>
 
-            <section className="flex justify-between smallDevice:flex-col gap-6 mediumDevice:flex-col mediumDevice:items-center">
+            <section className="flex justify-between smallDevice:flex-col gap-6 mediumDevice:flex-col mediumDevice:items-center largerDevice:flex-col largerDevice:items-center">
                {dataContentCard.map((dt) => (
                   <Card key={dt.id} {...dt} />
                ))}
